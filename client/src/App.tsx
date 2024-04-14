58b1ae2d6c59044fbce01755c768efef9553066d
Benedikt Nau <nau.benedikt@outlook.com>
2024-04-14 14:29:36 +0200

    Set Up a demo for testing backend requests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const callBackendAPI = async () => {
+      try {
+        const response = await fetch("/api");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const body = await response.json();
+        setData(body.message);
+      } catch (error) {
+        console.error();
+      }
+    };
+    callBackendAPI();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
+        <p style={{ color: "white" }}>{data}</p>
       </header>
     </div>
   );
 }
-
 export default App;
