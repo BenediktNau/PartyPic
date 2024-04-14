2d25cc7d23511acce268b7d0dc8741ec55006a1d
Benedikt Nau <nau.benedikt@outlook.com>
2024-04-14 23:36:51 +0200

    added Axios and tested the backend

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-    res.send({ message: "Hello from Express!" });
-  });
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+  res.send({ message: "Hello from Express!" });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
