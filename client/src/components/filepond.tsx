d82600a714929504a751f6492f57648e0d436cb3
Tobias Herbert <Tobias.M.Herbert@gmx.net>
2024-04-19 09:41:48 +0200

    WIP changing allignment and size of Upload Page

--- client/src/components/filepond.tsx
+++ client/src/components/filepond.tsx
@@ -11,30 +11,36 @@ import React, { useState } from "react";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-//import { useState } from "react";
 
 import "filepond/dist/filepond.min.css";
 import axios from "axios";
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
 //
 //
+
 function Filepond() {
-    const handleClick = () => {
-        axios.get(`http://localhost:3500/save`)
-    }
+  const handleClick = () => {
+    axios.get(`http://localhost:3500/save`);
+  };
 
   return (
-    <div className="w-screen">
-      <FilePond
-        allowMultiple={true}
-        maxFiles={3}
-        server="http://localhost:3500/upload"
-      />
-      <div className="flex justify-center">
-        <button onClick={handleClick} className="border-b-2 border-gray-700 hover:border-blue-500">Submit</button>
+      <div className="w-5/6 sm:w-2/5">
+        <FilePond
+          allowMultiple={true}
+          maxFiles={3}
+          server="http://localhost:3500/upload"
+        />
+        <div className="flex justify-center">
+          <button
+            onClick={handleClick}
+            className="border-b-2 border-gray-700 hover:border-blue-500"
+          >
+            Submit
+          </button>
+        </div>
       </div>
-    </div>
   );
 }
 
--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Upload() {
   return (
-    <div>
-      <FilePond />
+    <div className=" flex justify-center">
+          <FilePond />
     </div>
   );
 }
