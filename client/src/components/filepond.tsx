2f0f596b1dca0435c2c4e3496803476d5ea337a5
Benedikt Nau <nau.benedikt@outlook.com>
2024-04-20 17:46:35 +0200

    'Auth' with cookie

--- client/src/components/filepond.tsx
+++ client/src/components/filepond.tsx
@@ -13,18 +13,10 @@ import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orien
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 import "filepond/dist/filepond.min.css";
-import axios from "axios";
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-//
-//
-
 function Filepond() {
-  const handleClick = () => {
-    axios.get(`http://localhost:3500/save`).then((e) => console.log(e));
-  };
-
   return (
     <div className="w-5/6 sm:w-2/5">
       <FilePond
@@ -32,14 +24,6 @@ function Filepond() {
         maxFiles={3}
         server="http://localhost:3500/upload"
       />
-      <div className="flex justify-center">
-        <button
-          onClick={handleClick}
-          className="border-b-2 border-gray-700 hover:border-blue-500"
-        >
-          Submit
-        </button>
-      </div>
     </div>
   );
 }
