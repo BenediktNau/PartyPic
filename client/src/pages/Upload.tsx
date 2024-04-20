afb1c695b15f14436afa586e10ca163f9acd9094
Tobias Herbert <Tobias.M.Herbert@gmx.net>
2024-04-20 17:52:24 +0200

    WIP  Prompts

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,13 +1,15 @@
 import { useCookies } from "react-cookie";
 import FilePond from "../components/filepond";
 import React from "react";
+import RandomLineFetcher from "../components/promptHandling";
+
 
 function Upload() {
   return (
     <div className=" flex justify-center">
       <div className="w-5/6 sm:w-2/5">
         <div className="flex justify-center  ">
-          <div className="">placeHolder</div>
+          <RandomLineFetcher/>
         </div>
         <FilePond />
       </div>
