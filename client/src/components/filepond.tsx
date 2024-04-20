c2aa685e33a8ab5e40dc02125c4129b571a9e9ee
Tobias Herbert <Tobias.M.Herbert@gmx.net>
2024-04-20 23:42:07 +0200

    WIP Metadata dependencies

--- client/src/components/filepond.tsx
+++ client/src/components/filepond.tsx
@@ -11,23 +11,36 @@ import React, { useState } from "react";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import FilePondPluginFileMetadata from 'filepond-plugin-file-metadata';
+import { create } from "filepond";
 
 import "filepond/dist/filepond.min.css";
 import { styled } from "@mui/material";
 
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileMetadata);
+
+
 
 function Filepond() {
-  return (
+  
+    return (
     <div className=" w-5/6 sm:w-2/5">
       <FilePond
+
         credits={false}
         allowMultiple={true}
         maxFiles={3}
         server="http://localhost:3500/upload"
+        allowFileMetadata ={true}
+        
+        
       />
     </div>
+
+
+
   );
 }
+
 
 export default Filepond;
