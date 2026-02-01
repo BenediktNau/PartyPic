5f0fab91597db62a4e1f464959fd933bec01f440
Elias Niewelt <elias.nieweltwot@gmail.com>
2026-02-01 21:09:43 +0000

    maybe it will work....

---
+++ party-pic_server/src/metrics/metrics.constants.ts
@@ -0,0 +1,5 @@
+export const METRIC_APP_REQUEST_COUNT = 'app_request_count';
+export const METRIC_ACTIVE_SESSIONS = 'partypic_active_sessions_current';
+export const METRIC_SESSIONS_TOTAL = 'partypic_sessions_created_total';
+export const METRIC_PHOTOS_UPLOADED = 'partypic_photos_uploaded_total';
+export const METRIC_HTTP_DURATION = 'partypic_http_request_duration_seconds';
--- party-pic_server/src/metrics/metrics.module.ts
+++ party-pic_server/src/metrics/metrics.module.ts
@@ -3,11 +3,7 @@ import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { MetricsService } from './metrics.service';
 import { MetricsProvidersModule } from './metrics-providers.module';
 
-export const METRIC_APP_REQUEST_COUNT = 'app_request_count';
-export const METRIC_ACTIVE_SESSIONS = 'partypic_active_sessions_current';
-export const METRIC_SESSIONS_TOTAL = 'partypic_sessions_created_total';
-export const METRIC_PHOTOS_UPLOADED = 'partypic_photos_uploaded_total';
-export const METRIC_HTTP_DURATION = 'partypic_http_request_duration_seconds';
+export * from './metrics.constants';
 
 @Global()
 @Module({
