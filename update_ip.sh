a2a9b2d554c0afd7f47e1374f1c432ec73972685
Elias Niewelt <elias.nieweltwot@gmail.com>
2026-02-01 23:07:51 +0000

    updated update_ip to work from devcontainer

--- update_ip.sh
+++ update_ip.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
+set -e
 
-# 1. Den LoadBalancer Hostnamen aus Kubernetes holen
-LB_HOSTNAME=$(kubectl get ingress -n party-pic party-pic-server-ingress -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
-if [ -z "$LB_HOSTNAME" ]; then
-  exit 1
+if [ -d "terraform" ]; then
+    SSH_CMD=$(cd terraform && terraform output -raw ssh_command_server)
+else
+    exit 1
 fi
 
-echo "Hostname gefunden: $LB_HOSTNAME"
+if [ -z "$SSH_CMD" ]; then
+    exit 1
+fi
 
-# 2. Die IP-Adresse dahinter ermitteln (da nip.io eine IP braucht, keinen AWS-Namen)
+LB_HOSTNAME=$($SSH_CMD -o StrictHostKeyChecking=no -o LogLevel=QUIET "kubectl get ingress -n party-pic party-pic-server-ingress -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'")
+
+if [ -z "$LB_HOSTNAME" ]; then
+    exit 1
+fi
+
 LB_IP=$(dig +short $LB_HOSTNAME | head -n 1)
 
 if [ -z "$LB_IP" ]; then
-  exit 1
+    exit 1
 fi
 
-# Client
 sed -i "s/host: .*/host: app.$LB_IP.nip.io/" party-pic_client/chart/values.yaml
-# Server
 sed -i "s/host: .*/host: api.$LB_IP.nip.io/" party-pic_server/chart/values.yaml
 
-# 4. Automatisch pushen
-git add party-pic_client/chart/values.yaml party-pic_server/chart/values.yaml
-git commit -m "chore: auto-update ingress IP to $LB_IP"
-git push origin elias
-
+if ! git diff --quiet; then
+    git add party-pic_client/chart/values.yaml party-pic_server/chart/values.yaml
+    git commit -m "chore: auto-update ingress IP to $LB_IP via remote-kubectl"
+    git push origin elias
+fi
