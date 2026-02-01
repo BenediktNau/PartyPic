38e7ec394bb7a0a55837b8beb5416c98bb377dce
Elias Niewelt <elias.nieweltwot@gmail.com>
2026-02-01 19:16:38 +0000

    changed dependencies of metrics

--- party-pic_server/src/metrics/metrics.service.ts
+++ party-pic_server/src/metrics/metrics.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Gauge, Counter, Histogram } from 'prom-client';
 import { InjectMetric } from '@willsoto/nestjs-prometheus';
@@ -11,11 +12,11 @@ import {
 } from './metrics.module';
 
 @Injectable()
-export class MetricsService {
-  
+export class MetricsService implements OnModuleInit {
+  private sessionsDbService: SessionsDbService;
+
   constructor(
-    @Inject(forwardRef(() => SessionsDbService))
-    private readonly sessionsDbService: SessionsDbService,
+    private moduleRef: ModuleRef,
 
     @InjectMetric(METRIC_ACTIVE_SESSIONS) public readonly activeClientsGauge: Gauge<string>,
     @InjectMetric(METRIC_SESSIONS_TOTAL) public readonly totalSessionsCounter: Counter<string>,
@@ -23,8 +24,18 @@ export class MetricsService {
     @InjectMetric(METRIC_HTTP_DURATION) public readonly httpRequestDuration: Histogram<string>,
   ) {}
 
+  onModuleInit() {
+    try {
+      this.sessionsDbService = this.moduleRef.get(SessionsDbService, { strict: false });
+    } catch (error) {
+      console.error('MetricsService: Could not resolve SessionsDbService', error);
+    }
+  }
+
   @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron() {
+    if (!this.sessionsDbService) return;
+    
     try {
       const count = await this.sessionsDbService.countAllSessions();
       this.activeClientsGauge.set(count);
